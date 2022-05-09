8b9c7a8a260dcb4035775456720e4ca36481dbcf
Nataliya D <nataliya.danyoukova@student.howest.be>
2022-05-09 09:11:00 +0200

    Configure laravel project

---
+++ resources/js/app.js
@@ -0,0 +1,37 @@
+
+require('./bootstrap');
+
+
+import {createApp} from 'vue';
+import LoginForm from "./components/LoginForm";
+import FooterView from "./components/FooterView";
+import HeaderComponent from "./components/HeaderComponent";
+import ProductCard from "./components/ProductCard";
+import ProductView from "./components/ProductView";
+import router from "./router/router";
+import testComponent from "./components/testComponent";
+
+
+
+
+/*
+
+createApp(App)
+    .use(router)
+    .use(store)
+    .mount('#app')
+*/
+
+const app = createApp({});
+
+app.component('LoginForm', LoginForm);
+app.component('FooterView', FooterView);
+app.component('HeaderComponent', HeaderComponent);
+app.component('testComponent', testComponent);
+app.component('ProductCard', ProductCard);
+app.component('ProductView', ProductView)
+app.use(router);
+//app.use(store);
+//app.use(axios);
+
+app.mount('#app');
